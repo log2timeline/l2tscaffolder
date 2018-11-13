a340c2376517a3a572314e73b650df8333ae2aa9
Kristinn Gudjonsson <kristinn@log2timeline.net>
2018-11-13 06:25:57 -0800

    Adding a TS analyzer scaffolders.

--- plasoscaffolder/scaffolders/__init__.py
+++ plasoscaffolder/scaffolders/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 """This file imports Python modules that registers scaffolders."""
 from plasoscaffolder.scaffolders import plaso_sqlite
+from plasoscaffolder.scaffolders import timesketch_index
+from plasoscaffolder.scaffolders import timesketch_sketch
---
+++ plasoscaffolder/scaffolders/timesketch.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""Timesketch scaffolder that generates analyser plugins."""
+import os
+import logging
+
+from typing import Dict
+from typing import Iterator
+from typing import List
+from typing import Tuple
+
+from plasoscaffolder.lib import definitions
+from plasoscaffolder.lib import errors
+from plasoscaffolder.lib import mapping_helper
+from plasoscaffolder.scaffolders import interface
+
+
+class TimesketchBaseScaffolder(interface.Scaffolder):
+  """The Timesketch base scaffolder interface.
+
+  Attributes:
+    class_name (str): class name of the Timesketch analyzer to be generated.
+  """
+
+  # The name of the plugin this scaffolder plugin provides.
+  NAME = 'timesketch_base'
+
+  # One liner describing what the scaffolder provides.
+  DESCRIPTION = 'This is a scaffolder for Timesketch analyzers'
+
+  # Define which project this particular scaffolder belongs to.
+  PROJECT = definitions.DEFINITION_TIMESKETCH
+
+  # Filename of templates.
+  TEMPLATE_PLUGIN_FILE = ''
+  TEMPLATE_PLUGIN_TEST = ''
+
+  # Questions, a list that contains all the needed questions that the
+  # user should be prompted about before the plugin or parser is created.
+  # Each element in the list should be of the named tuple question.
+  QUESTIONS = []
+
+  def __init__(self):
+    """Initializes the Timesketch scaffolder."""
+    super(TimesketchBaseScaffolder, self).__init__()
+    self._plugin_path = os.path.join('timesketch', 'lib', 'analyzers')
+    self._plugin_test_path = os.path.join('timesketch', 'lib', 'analyzers')
+    self._mapping_helper = mapping_helper.ParserMapper()
+    self._mapping_helper.SetDefaultPaths()
+
+    self.class_name = ''
+
+  def _GeneratePlugin(self) -> str:
+    """Generates the plugin file."""
+    return self._mapping_helper.RenderTemplate(
+        self.TEMPLATE_PLUGIN_FILE, self.GetJinjaContext())
+
+  def _GeneratePluginTest(self) -> str:
+    """Generates the plugin test file."""
+    return self._mapping_helper.RenderTemplate(
+        self.TEMPLATE_PLUGIN_TEST, self.GetJinjaContext())
+
+  def GetFilesToCopy(self) -> Iterator[Tuple[str, str]]:
+    """Return a list of files that need to be copied.
+
+    Yields:
+      tuple (str, str): file name of source and destination.
+    """
+    return iter(())
+
+  def GetJinjaContext(self) -> Dict[str, object]:
+    """Returns a dict that can be used as a context for Jinja2 templates.
+
+    Returns:
+      dict: containing:
+        str: name of Jinja argument.
+        object: Jinja argument value.
+    """
+    context = super(TimesketchBaseScaffolder, self).GetJinjaContext()
+    context['class_name'] = self.class_name
+    context['plugin_name'] = self._output_name
+
+    return context
+
+  def GenerateFiles(self) -> Iterator[Tuple[str, str]]:
+    """Generates all the files required for a Timesketch analyser plugin.
+
+    Yields:
+      list[tuple]: containing:
+       str: file name.
+       str: file content.
+    """
+    plugin_name = '{0:s}.py'.format(self._output_name)
+
+    self.class_name = self._mapping_helper.GenerateClassName(
+        self._output_name)
+
+    try:
+      plugin_path = os.path.join(self._plugin_path, plugin_name)
+      plugin_content = self._GeneratePlugin()
+      yield plugin_path, plugin_content
+    except SyntaxError as exception:
+      logging.error((
+          'Syntax error while attempting to generate plugin, error '
+          'message: {0!s}').format(exception))
+
+    test_file_name = '{0:s}_test.py'.format(self._output_name)
+    test_path = os.path.join(self._plugin_test_path, test_file_name)
+    try:
+      test_content = self._GeneratePluginTest()
+      yield test_path, test_content
+    except SyntaxError as exception:
+      logging.error((
+          'Syntax error while attempting to generate plugin test, error '
+          'message: {0!s}').format(exception))
+
+    plugin_string = (
+        '# TODO: put in alphabetical order.\nfrom {0:s} import {1:s}').format(
+            self._plugin_path.replace(os.sep, '.'), self._output_name)
+    plugin_init_path = os.path.join(self._plugin_path, '__init__.py')
+    yield plugin_init_path, plugin_string
