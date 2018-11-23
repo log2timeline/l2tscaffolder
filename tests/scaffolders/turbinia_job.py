2c0949816bd6efaf7c4d52a58a4c266170f4ffc0
Kristinn Gudjonsson <kristinn@log2timeline.net>
2018-11-23 07:45:51 -0800

    Adding tests.

---
+++ tests/scaffolders/turbinia_job.py
@@ -0,0 +1,44 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Tests for the Turbinia job scaffolder."""
+import unittest
+
+from l2tscaffolder.scaffolders import turbinia_job
+
+
+class TurbiniaJobScaffolderTest(unittest.TestCase):
+  """Test class for the Turbinia job scaffolder."""
+
+  maxDiff = None
+
+  def testTurbiniaJobScaffolder(self):
+    """Test the Turbinia job scaffolder."""
+    scaffolder = turbinia_job.TurbiniaJobScaffolder()
+    scaffolder.SetOutputName('secret_processing')
+
+    file_copy_paths = [x for _, x in scaffolder.GetFilesToCopy()]
+    self.assertEqual(file_copy_paths, [])
+
+    files_generated = dict(scaffolder.GenerateFiles())
+
+    expected_files = frozenset([
+        'turbinia/jobs/secret_processing.py',
+        'turbinia/jobs/__init__.py'])
+    self.assertEqual(set(files_generated.keys()), expected_files)
+
+    expected_parser_init_addition = (
+        '# TODO: put in alphabetical order.\nfrom '
+        'turbinia.jobs import secret_processing')
+    self.assertEqual(
+        expected_parser_init_addition,
+        files_generated['turbinia/jobs/__init__.py'])
+
+    with open('test_data/turbinia_job_output.py', 'r') as fh:
+      expected_parser_content = fh.read()
+    self.assertEqual(
+        expected_parser_content,
+        files_generated['turbinia/jobs/secret_processing.py'])
+
+
+if __name__ == '__main__':
+  unittest.main()
