6fe7a0c19c6f7caf5a1eb9c7e1d607b7b6ce5091
Kristinn Gudjonsson <kristinn@log2timeline.net>
2019-01-03 04:39:37 -0800

    Upgrading version.

--- tests/other/test_version.py
+++ tests/other/test_version.py
@@ -12,7 +12,7 @@ class VersionTest(unittest.TestCase):
   def testGetVersion(self):
     """testing the get version"""
     actual = l2tscaffolder.__version__
-    self.assertEqual('20181127', actual)
+    self.assertEqual('20190103', actual)
 
 
 if __name__ == '__main__':
