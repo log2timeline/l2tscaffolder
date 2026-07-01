06e03e5bae1eac9e56fce8b102bc2c82ae2c5595
Janosch Koepper <jkoepper@google.com>
2026-07-01 09:43:47 +0000

    Update Timesketch scaffolder tests with new expected outputs matching OpenSearch templates

---
+++ test_data/timesketch_index_testing_plugin.py
@@ -0,0 +1,19 @@
+"""Tests for TestingPlugin."""
+import mock
+
+from timesketch.lib.analyzers import testing
+from timesketch.lib.testlib import BaseTest
+from timesketch.lib.testlib import MockDataStore
+
+
+class TestTesting(BaseTest):
+    """Tests the functionality of the analyzer."""
+
+    # Mock the Opensearch datastore.
+    @mock.patch(
+        u'timesketch.lib.analyzers.interface.OpenSearchDataStore',
+        MockDataStore)
+    def test_Testing_analyzer_class(self):
+        """Test core functionality of the analyzer class."""
+        # TODO: Write actual tests here.
+        self.assertEqual(True, False)
